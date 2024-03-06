a2b9582b38c5b12faaa595bba49bf48bf780f866
mvlabat <mvlabat@gmail.com>
2024-03-06 19:05:29 +0200

    Fix urls

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,10 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback: 'app.html'
+      fallback: 'index.html'
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/gnosis-public-key-recover' : '',
+      base: process.env.NODE_ENV === 'production' ? '/gnosis-pubkey-recover' : '',
     },
   }
 };
